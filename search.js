c90c8c91be17f2f233d64b381d1dc0ac76c4849e
NightmareZ <m.m.makarov@gmail.com>
2015-05-14 10:22:55 +0300

    remove requirejs and change encoding to utf8

--- search.js
+++ search.js
@@ -1,161 +1,157 @@
-define('search', [], function() {
-    (function ($) {
-        /*
-         * func - function (textFieldValue, function (keyValuesResult))
-         * callback - function (keyResult)
-         *
-         */
-        $.fn.searchPlugin = function (func, callback) {
-            var input = this;
+﻿(function ($) {
+    /*
+     * func - function (textFieldValue, function (keyValuesResult))
+     * callback - function (keyResult)
+     *
+     */
+    $.fn.searchPlugin = function (func, callback) {
+        var input = this;
 
-            this.focusout(function () {
-                $('#searchPluginList').remove();
-            });
+        this.focusout(function () {
+            $('#searchPluginList').remove();
+        });
 
-            this.keyup(function (e) {
-                switch (e.which) {
-                    case 38 /* up key */:
-                        var ul = $('#searchPluginList');
+        this.keyup(function (e) {
+            switch (e.which) {
+                case 38 /* up key */:
+                    var ul = $('#searchPluginList');
 
-                        if (!ul.length) {
-                            return;
-                        }
+                    if (!ul.length) {
+                        return;
+                    }
 
-                        var list = ul.find('li');
-                        var finded = false;
+                    var list = ul.find('li');
+                    var finded = false;
 
-                        for (var j = list.length - 1; j >= 0; --j) {
-                            if ($(list[j]).hasClass('searchPluginListSelected')) {
-                                if (j > 0) {
-                                    $(list[j]).removeClass('searchPluginListSelected');
-                                    $(list[j - 1]).addClass('searchPluginListSelected');
-                                }
-
-                                finded = true;
-                                break;
+                    for (var j = list.length - 1; j >= 0; --j) {
+                        if ($(list[j]).hasClass('searchPluginListSelected')) {
+                            if (j > 0) {
+                                $(list[j]).removeClass('searchPluginListSelected');
+                                $(list[j - 1]).addClass('searchPluginListSelected');
                             }
+
+                            finded = true;
+                            break;
                         }
+                    }
 
-                        if (!finded && list.length > 0) {
-                            $(list[list.length - 1]).addClass('searchPluginListSelected');
-                        }
+                    if (!finded && list.length > 0) {
+                        $(list[list.length - 1]).addClass('searchPluginListSelected');
+                    }
 
-                        break;
+                    break;
 
-                    case 40 /* down key */:
-                        var ul = $('#searchPluginList');
+                case 40 /* down key */:
+                    var ul = $('#searchPluginList');
 
-                        if (!ul.length) {
-                            return;
-                        }
+                    if (!ul.length) {
+                        return;
+                    }
 
-                        var list = ul.find('li');
-                        var finded = false;
+                    var list = ul.find('li');
+                    var finded = false;
 
-                        for (var j = 0; j < list.length; ++j) {
-                            if ($(list[j]).hasClass('searchPluginListSelected')) {
-                                if (j < list.length - 1) {
-                                    $(list[j]).removeClass('searchPluginListSelected');
-                                    $(list[j + 1]).addClass('searchPluginListSelected');
-                                }
-
-                                finded = true;
-                                break;
+                    for (var j = 0; j < list.length; ++j) {
+                        if ($(list[j]).hasClass('searchPluginListSelected')) {
+                            if (j < list.length - 1) {
+                                $(list[j]).removeClass('searchPluginListSelected');
+                                $(list[j + 1]).addClass('searchPluginListSelected');
                             }
+
+                            finded = true;
+                            break;
                         }
+                    }
 
-                        if (!finded && list.length > 0) {
-                            $(list[0]).addClass('searchPluginListSelected');
-                        }
+                    if (!finded && list.length > 0) {
+                        $(list[0]).addClass('searchPluginListSelected');
+                    }
 
-                        break;
+                    break;
 
-                    case 13 /* enter */:
-                        var input = $(this);
-                        var ul = $('#searchPluginList');
+                case 13 /* enter */:
+                    var input = $(this);
+                    var ul = $('#searchPluginList');
 
-                        if (!ul.length) {
-                            return;
-                        }
+                    if (!ul.length) {
+                        return;
+                    }
 
-                        var list = ul.find('li');
+                    var list = ul.find('li');
 
-                        for (var j = 0; j < list.length; ++j) {
-                            if ($(list[j]).hasClass('searchPluginListSelected')) {
-                                input.val($(list[j]).text());
-                                var key = $(list[j]).data('item-id');
-                                input.data('item-id', key);
+                    for (var j = 0; j < list.length; ++j) {
+                        if ($(list[j]).hasClass('searchPluginListSelected')) {
+                            input.val($(list[j]).text());
+                            var key = $(list[j]).data('item-id');
+                            input.data('item-id', key);
 
-                                if (callback) {
-                                    callback(key);
-                                }
-
-                                ul.remove();
-                                break;
+                            if (callback) {
+                                callback(key);
                             }
+
+                            ul.remove();
+                            break;
                         }
+                    }
 
-                        break;
+                    break;
 
-                    default:
-                        var input = $(this);
-                        var ul = $('#searchPluginList');
+                default:
+                    var input = $(this);
+                    var ul = $('#searchPluginList');
 
-                        if (!ul.length) {
-                            ul = $(document.createElement('ul'))
-                                .css('position', 'absolute')
-                                .css('list-style', 'none')
-                                .css('background-color', 'white')
-                                .css('margin', '0')
-                                .css('padding', '0')
-                                .attr('id', 'searchPluginList');
-                            $('body').append(ul);
+                    if (!ul.length) {
+                        ul = $(document.createElement('ul'))
+                            .css('position', 'absolute')
+                            .css('list-style', 'none')
+                            .css('background-color', 'white')
+                            .css('margin', '0')
+                            .css('padding', '0')
+                            .attr('id', 'searchPluginList');
+                        $('body').append(ul);
+                    }
+
+                    ul.css('top', input.offset().top + input.outerHeight() + 'px')
+                        .css('left', input.offset().left - 1 + 'px')
+                        .css('width', input.outerWidth() + 'px');
+
+                    ul.empty();
+
+                    func(input.val(), function (result) {
+                        if (_.size(result)) {
+                            _.each(result, function (value, key) {
+                                var li = $(document.createElement('li'))
+                                    .css('margin', '0')
+                                    .css('padding', '8px')
+                                    .css('padding-left', '12px')
+                                    .css('font-family', 'sans-serif')
+                                    .css('font-size', '14px')
+                                    .text(value);
+                                li.data('item-id', key);
+                                ul.append(li);
+
+                                (function (ul, key, input) {
+                                    li.mousedown(function () {
+                                        input.val($(this).text().trim());
+                                        var key = $(this).data('item-id');
+                                        input.data('item-id', key);
+                                        ul.remove();
+
+                                        if (callback) {
+                                            callback(key);
+                                        }
+                                    });
+                                })(ul, key, input);
+                            });
+                        } else {
+                            ul.remove();
                         }
+                    });
 
-                        ul.css('top', input.offset().top + input.outerHeight() + 'px')
-                            .css('left', input.offset().left - 1 + 'px')
-                            .css('width', input.outerWidth() + 'px');
+                    break;
+            }
+        });
 
-                        ul.empty();
-
-                        func(input.val(), function (result) {
-                            if (_.size(result)) {
-                                _.each(result, function (value, key) {
-                                    var li = $(document.createElement('li'))
-                                        .css('margin', '0')
-                                        .css('padding', '8px')
-                                        .css('padding-left', '12px')
-                                        .css('font-family', 'sans-serif')
-                                        .css('font-size', '14px')
-                                        .text(value);
-                                    li.data('item-id', key);
-                                    ul.append(li);
-
-                                    (function (ul, key, input) {
-                                        li.mousedown(function () {
-                                            input.val($(this).text().trim());
-                                            var key = $(this).data('item-id');
-                                            input.data('item-id', key);
-                                            ul.remove();
-
-                                            if (callback) {
-                                                callback(key);
-                                            }
-                                        });
-                                    })(ul, key, input);
-                                });
-                            } else {
-                                ul.remove();
-                            }
-                        });
-
-                        break;
-                }
-            });
-
-            return this;
-        };
-    })(jQuery);
-
-    return jQuery;
-});
+        return this;
+    };
+})(jQuery);
